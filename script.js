f1984f96c35c4012d60bcb3ccbcbc9b3d2080e1f
Kutlwano <lebomoseki15@gmail.com>
2024-01-07 09:59:11 +0200

    refactored  script.js to separate different concerns into smaller functions

--- script.js
+++ script.js
@@ -5,41 +5,56 @@ infoText = wrapper.querySelector(".info-text"),
 volumeIcon = wrapper.querySelector(".word ")
 let audio;
 
-function data(result, word){
-    if(result.title){
-        infoText.innerHTML = `Cannot find the meaning of <span>${word}</span>. Please, try to search for another word`
-    }else{
-        console.log(result);
+function setInfoText(message, color = "#000") {
+    infoText.style.color = color;
+    infoText.innerHTML = message;
+}
+
+function setSynonyms(synonymsList) {
+    if (!synonymsList || synonymsList.length === 0) {
+        synonyms.parentElement.style.display = "none";
+    } else {
+        synonyms.parentElement.style.display = "block";
+        synonyms.innerHTML = "";
+        for (let i = 0; i < Math.min(synonymsList.length, 5); i++) {
+            let tag = `<span>${synonymsList[i]}</span>`;
+            synonyms.insertAdjacentHTML("beforeend", tag);
+        }
+    }
+}
+
+function playAudio(audioUrl) {
+    audio = new Audio("https:" + audioUrl);
+    audio.play();
+}
+
+function handleApiResponse(result, word) {
+    if (result.title) {
+        setInfoText(`Cannot find the meaning of <span>${word}</span>. Please, try to search for another word.`);
+    } else {
         wrapper.classList.add("active");
         let definitions = result[0].meanings[0].definitions[0],
-        phonetics = `${result[0].meanings[0].partOfSpeech} /${result[0].phonetics[0].text}/`;
+            phonetics = `${result[0].meanings[0].partOfSpeech} /${result[0].phonetics[0].text}/`;
 
         document.querySelector(".word p").innerText = result[0].word;
         document.querySelector(".word span").innerText = phonetics;
         document.querySelector(".meaning span").innerText = definitions.definition;
         document.querySelector(".example span").innerText = definitions.example;
-        audio = new Audio("https:" + result[0].phonetics[0].audio);
-
-        if(definitions.synonyms[0] == undefined){
-            synonyms.parentElement.style.display = "none";
-        }else{
-            synonyms.parentElement.style.display = "block";
-            synonyms.innerHTML = "";
-            for (let i = 0; i < 5; i++) {
-                let tag = `<span>${definitions.synonyms[i]}</span>`;
-                synonyms.insertAdjacentHTML("beforeend", tag);
-            }
-        }
-
+        playAudio(result[0].phonetics[0].audio);
+        setSynonyms(definitions.synonyms);
     }
 }
 
-function fetchApi(word){
-    infoText.style.color = "#000";
-    infoText.innerHTML = `Searhing the meaning of <span>${word}</span>`;
+function fetchApi(word) {
+    setInfoText(`Searching the meaning of <span>${word}</span>`);
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
-    fetch(url).then(res => res.json()).then(result => data(result,word));
-
+    fetch(url)
+        .then(res => res.json())
+        .then(result => handleApiResponse(result, word))
+        .catch(error => {
+            console.error("Error fetching data:", error);
+            setInfoText("An error occurred while fetching the data. Please try again later.", "red");
+        });
 }
 
 searchInput.addEventListener("keyup", e =>{
