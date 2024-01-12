7763bbd19be9b2642382e7a81dee762c6cc5873b
Kutlwano <lebomoseki15@gmail.com>
2024-01-12 14:48:32 +0200

    modified script.js

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const wrapper = document.querySelector(".wrapper"),
 searchInput = wrapper.querySelector("input"),
 synonyms = wrapper.querySelector(".synonyms .list"),
 infoText = wrapper.querySelector(".info-text"),
-volumeIcon = wrapper.querySelector(".fa-volume-up")
+volumeIcon = wrapper.querySelector(".word i"),
+exitIcon = wrapper.querySelector(".search span");
 let audio;
 
 
@@ -18,16 +19,16 @@ function setSynonyms(synonymsList) {
         synonyms.parentElement.style.display = "block";
         synonyms.innerHTML = "";
         for (let i = 0; i < Math.min(synonymsList.length, 5); i++) {
-            let tag = `<span>${synonymsList[i]}</span>`;
+            let tag = `<span onclick= search('${synonymsList[i]}')>${synonymsList[i]}</span>`;
             synonyms.insertAdjacentHTML("beforeend", tag);
         }
     }
 }
 
-function playAudio(audioUrl) {
-    audio = new Audio("https:" + audioUrl);
-    audio.play();
-}
+// function playAudio(audioUrl) {
+//     audio = new Audio("https:" + audioUrl);
+//     audio.play();
+// }
 
 function handleApiResponse(result, word) {
     if (result.title) {
@@ -42,14 +43,23 @@ function handleApiResponse(result, word) {
         document.querySelector(".meaning span").innerText = definitions.definition;
         document.querySelector(".example span").innerText = definitions.example;
         // playAudio(result[0].phonetics[0].audio);
+        // volumeIcon.addEventListener("click", () => {
+        //     playAudio(result[0].phonetics[0].audio);
+        //   });
+        audio = new Audio("https:" + result[0].phonetic[0].audio);
+
         setSynonyms(definitions.synonyms);
-        volumeIcon.addEventListener("click", () => {
-            playAudio(result[0].phonetics[0].audio);
-          });
     }
 }
 
+function search(word){
+    searchInput.value = word;
+    fetchApi(word);
+    wrapper.classList.remove("active");
+}
+
 function fetchApi(word) {
+    wrapper.classList.remove("active");
     setInfoText(`Searching the meaning of <span>${word}</span>`);
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
     fetch(url)
@@ -66,3 +76,13 @@ searchInput.addEventListener("keyup", e =>{
         fetchApi(e.target.value);
     }
 });
+
+volumeIcon.addEventListener("click", ()=>{
+    audio.play();
+  });
+
+  exitIcon.addEventListener("click", ()=>{
+    searchInput.value = "";
+    searchInput.focus();
+    wrapper.classList.remove("active");
+  })
